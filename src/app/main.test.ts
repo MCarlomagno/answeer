eae61196248258408267ebbeeb3938406ccf65e0
mcarlomagno <marcoscarlomagno1@gmail.com>
2021-03-04 21:43:21 -0300

    test + dependencies: added some tests and puppeteer dependency

--- src/app/main.test.ts
+++ src/app/main.test.ts
@@ -1,6 +1,33 @@
+import { AnswerService, MockAnswerService } from '../services/answer.service';
+import { MockTextService } from '../services/search.service';
+import { Main } from './main';
 
+describe('main', () => {
+  // setup dummy services
+  const answerService = new MockAnswerService();
+  const searchService = new MockTextService();
+  const main = new Main(answerService, searchService);
 
-describe('setup', () => {
-    it('should exist', async () => {
+  describe('showLoader', () => {
+    it('should return "visible" if true', async () => {
+      document.body.innerHTML = `<div id="loader" class="loader"></div>`;
+      main.setupDOM();
+      const visibility = main.showLoader(true);
+      expect(visibility).toBe('visible');
     });
+
+    it('should return "hidden" if false', async () => {
+      document.body.innerHTML = `<div id="loader" class="loader"></div>`;
+      main.setupDOM();
+      const visibility = main.showLoader(false);
+      expect(visibility).toBe('hidden');
+    });
+
+    it('should return "undefined" if the element does not exist', async () => {
+      document.body.innerHTML = '';
+      main.setupDOM();
+      const visibility = main.showLoader(true);
+      expect(visibility).toBeFalsy();
+    });
+  });
 });
