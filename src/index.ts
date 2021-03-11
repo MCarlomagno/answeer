f482e88c4072378b7f4ff61b2feabf036ca700e1
mcarlomagno <marcoscarlomagno1@gmail.com>
2021-03-11 17:39:48 -0300

    feat: text inputs ui

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { Main } from './app/main';
 import { QuestionAndAnswer } from './services/question-and-answer';
 import { Wikipedia } from './services/wikipedia';
+import {MDCTextField} from '@material/textfield';
+
+const textElements = document.querySelectorAll('.mdc-text-field');
+if(textElements) {
+    textElements.forEach((el) => {
+        const textField = new MDCTextField(el);
+    })
+}
+
 
 const wikipedia = new Wikipedia();
 const questionAndAnswer = new QuestionAndAnswer();
